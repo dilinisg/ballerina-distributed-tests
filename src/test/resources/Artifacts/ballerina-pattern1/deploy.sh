0157b7c076b0eef2718eda8a6e56f2c8790cd935
jae <jae@swear.com>
2017-05-09 11:11:25 +0530

    Commiting scripts

--- src/test/resources/Artifacts/ballerina-pattern1/deploy.sh
+++ src/test/resources/Artifacts/ballerina-pattern1/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 prgdir=$(dirname "$0")
 script_path=$(cd "$prgdir"; pwd)
+echo $KUBERNETES_MASTER
 echo "Creating the Pods!!!!"
 kubectl create -f $script_path/ballerina_test_service.yaml
 kubectl create -f $script_path/ballerina_test_rc.yaml
--- src/test/resources/Artifacts/ballerina-pattern1/undeploy.sh
+++ src/test/resources/Artifacts/ballerina-pattern1/undeploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-secho "Setting Kubernetes Master"
+echo $KUBERNETES_MASTER
+echo "Setting Kubernetes Master"
 echo $KUBERNETES_MASTER
 echo "Removing the K8S PODS!!!!!"
 kubectl delete rc,services,pods -l name="ballerina-test-version-1"
