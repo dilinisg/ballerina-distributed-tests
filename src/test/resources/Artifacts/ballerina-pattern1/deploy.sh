1af7f66efb59c296e49af5fb103b4c94f4dbe52b
jae <jae@swear.com>
2017-05-09 11:13:24 +0530

    Commiting scripts

--- src/test/resources/Artifacts/ballerina-pattern1/deploy.sh
+++ src/test/resources/Artifacts/ballerina-pattern1/deploy.sh
@@ -2,6 +2,7 @@
 prgdir=$(dirname "$0")
 script_path=$(cd "$prgdir"; pwd)
 echo $KUBERNETES_MASTER
+echo $script_path
 echo "Creating the Pods!!!!"
 kubectl create -f $script_path/ballerina_test_service.yaml
 kubectl create -f $script_path/ballerina_test_rc.yaml
